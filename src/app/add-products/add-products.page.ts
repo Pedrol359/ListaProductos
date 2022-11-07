12b69fee90c095c3edd0f808f30cd44ffd91d1d4
Pedro Avila <84610112+Pedrol359@users.noreply.github.com>
2022-11-07 08:53:00 -0700

    detallitos

--- src/app/add-products/add-products.page.ts
+++ src/app/add-products/add-products.page.ts
@@ -25,7 +25,8 @@ export class AddProductsPage implements OnInit {
     //this.producto.nombre="";
     this.ionicForm = this.formBuilder.group({
       nombre: ['', [Validators.required, Validators.minLength(2)]],
-      precio: ['', [Validators.required, Validators.pattern('([0-9])+(\.([0-9]{1,2}))?')]]
+      precio: ['', [Validators.required, Validators.pattern('([0-9])+(\.([0-9]{1,2}))?')]],
+      imagen: ['', []]
     })
   }
   submitForm() {
@@ -37,7 +38,8 @@ export class AddProductsPage implements OnInit {
     } else {
       this.producto={
         nombre:this.ionicForm.value.nombre.toString(),
-        precio:Number(this.ionicForm.value.precio)
+        precio:Number(this.ionicForm.value.precio),
+        foto:this.ionicForm.value.imagen.toString()
       };
       //this.producto.nombre=this.ionicForm.value.nombre.toString();
       //this.producto.precio=Number(this.ionicForm.value.precio);
